42f9e953d861cdbbf6e7f9b6f8576f8ff1ae9488
denis.ibanez@outlook.com <denis.ibanez@outlook.com>
2025-02-20 22:59:35 +0000

    feat: insert translate

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['upload.wikimedia.org'],
+    domains: [],
   },
 };
 
