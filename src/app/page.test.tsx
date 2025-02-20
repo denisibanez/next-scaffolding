7b7715b6b0216b66c3024dc843ff8f88761efb6b
denis.ibanez@outlook.com <denis.ibanez@outlook.com>
2025-02-20 20:15:15 +0000

    feat: insert global state management

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,4 +1,3 @@
-
 import { render, screen } from '@testing-library/react';
 import Page from '@/app/page';
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+'use client';
+
 import { Button } from 'antd';
+import { useStore } from '../store/example/example.store';
 
 export default function Home() {
+  const { count, inc } = useStore();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Button type="primary">Button</Button>
+        <Button type="primary" onClick={() => inc()}>Button</Button>
+
+        <div className="grid">
+          Contador: {count}
+        </div>
       </main>
     </div>
   );
