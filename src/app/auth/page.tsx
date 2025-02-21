204696103242ec9e9c69e8b90623056c3bd1b6b8
denis.ibanez@outlook.com <denis.ibanez@outlook.com>
2025-02-21 03:26:32 +0000

    feat: insert next auth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -19,18 +19,16 @@ export default function AuthView() {
   }
 
   return (
+    <section className="w-full flex items-center  min-h-screen">
+      <div className="rounded p-52 flex items-center justify-between mx-auto shadow-sm bg-white">
+        {status === 'loading' && <span> Carregando...</span>}
 
-        <section className="w-full flex items-center  min-h-screen">
-          <div className="rounded p-52 flex items-center justify-between mx-auto shadow-sm bg-white">
-            {status === 'loading' && <span> Carregando...</span>}
-
-            {status === 'unauthenticated' && (
-              <Button type="primary" onClick={handleLogin}>
-                Login
-              </Button>
-            )}
-          </div>
-        </section>
-
+        {status === 'unauthenticated' && (
+          <Button type="primary" onClick={handleLogin}>
+            Login
+          </Button>
+        )}
+      </div>
+    </section>
   );
 }
